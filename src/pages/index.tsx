4bd60d4b2837ee230e1fa73db29ea68e9443be1b
Rodrigo Gonçalves Rebelo <rogrebelo@gmail.com>
2021-09-28 07:55:36 -0300

    Componentes Principal _document, cabeçalho e post. Importação da logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import { GetStaticProps } from 'next';
-
 import { getPrismicClient } from '../services/prismic';
-
+import { FiCalendar, FiUser } from 'react-icons/fi'
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
+import Header from '../components/Header';
+import Link from 'next/link';
 interface Post {
   uid?: string;
   first_publication_date: string | null;
@@ -24,9 +25,33 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({postsPagination}) {
+  return (
+    <>
+      <main className={commonStyles.container}>
+        <Header />
+        <div className={styles.posts}>
+          <Link href="/">
+            <a className={styles.post}>
+              <strong>Título Qualquer</strong>
+              <p>Pensando em sincronização em vez de ciclos de vida.</p>
+              <ul>
+                <li>
+                  <FiCalendar />
+                  15 mar 2021
+                </li>
+                <li>
+                  <FiUser />
+                  15 mar 2021
+                </li>
+              </ul>
+            </a>
+          </Link>
+        </div>
+      </main>
+    </>
+  )
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
